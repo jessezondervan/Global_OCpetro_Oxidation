4fd61b8c60fd93f398aa0276cb48460cd0b10130
Dr Jesse Zondervan <36923725+jessezondervan@users.noreply.github.com>
2023-01-26 18:04:39 +0000

    Add files via upload

---
+++ job_script_example.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+#SBATCH --job-name=Global_cal 
+#SBATCH --partition=short
+#SBATCH --time=00:45:00 
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=48
+
+cd $TMPDIR || exit 1
+
+rsync -av $DATA/input_global ./
+
+module purge
+module load Anaconda3//2021.11
+
+source activate $DATA/carbon-env-newer
+
+python3 input_global/Glob_newmethod_parr_globalresidual.py
+
+rsync -av --exclude=input_global --exclude='glob_petrodenud.tif' --exclude='glob_petrodenud_wo_alluv.tif' ./ $DATA
